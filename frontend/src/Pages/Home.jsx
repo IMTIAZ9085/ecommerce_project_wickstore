4028e7960bc1a7defe47fbf750295d147f74fd15
SK IMTIAZ ALI <81345763+IMTIAZ9085@users.noreply.github.com>
2022-08-07 23:59:07 +0530

    useReducer IMPLEMENTED

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,34 +1,61 @@
-import React, { useState ,useEffect} from 'react'
+import React, { useState ,useEffect, useReducer} from 'react'
 import { Link } from 'react-router-dom';
 // import data from '../data';
 import axios from 'axios';
 
+//REDUCER FUNCTION FOR STATE MANAGEMENT
+const reducer = (state,action) => {
+  switch (action.type){
+    case 'FETCH_REQUEST':
+      return {...state,loading: true};
+    case 'FETCH_SUCCESS':
+      return {...state,products:action.payload,loading: true};
+    case 'FETCH_FAIL':
+      return {...state,error: action.payload};
+    default: 
+      return state;
+  }
+}
+
 const Home = () => {
 
-  const [products, setProducts] = useState([]);
+  // const [productslist, setProducts] = useState([]);
+
+  const [{loading,error,products},dispatch] = useReducer(reducer,{
+    loading: true,
+    error:'',
+    products:[],
+  });
  
   useEffect(() =>{
     const fetchData = async() =>{
-      const result = await axios.get('/api/products');
-      // const dt =result.data ;
-      setProducts(result.data);
-      // console.log(result.data);
+      dispatch({type:'FETCH_REQUEST'});
+      try{
+        const result = await axios.get('/api/products');
+        dispatch({type:'FETCH_SUCCESS',payload:result.data.products})
+      }catch(e){
+        dispatch({type:'FETCH_FAIL',payload:e.message});
+      }
+
+      // setProducts(result.data.products);
+      // console.log(result);
       // console.log(products);
   };
    fetchData();
   },[]);
 
-  console.log(products);
 
   return (
     <div>
     
     <h1>Features Products</h1>
       <div className="products_list">
-       {products.map((product)=>{
-          return (
+        {/* <p style={{color: 'green'}}>{productslist.products[0].name}</p> */}
+       {
+        products.map((product)=>
+           (
+            
             <div className="each_product" key={product.slug}>
-
            <Link to={`/product/${product.slug}`}>
             <img style={{height:"14rem",width:"14rem"}} src={product.image} alt={product.name} />
             </Link>
@@ -43,7 +70,7 @@ const Home = () => {
             </div>
           </div>
       )    
-       })
+       )
        }
        </div>
     </div>
